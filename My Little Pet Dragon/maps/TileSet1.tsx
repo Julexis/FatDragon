1cdb66980d5544e55621915c56c60eca69d844e9
julexis <julexislavoie12@gmail.com>
2019-04-30 13:27:36 -0400

    Create TileSet1.tsx

---
+++ My Little Pet Dragon/maps/TileSet1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="TileSet1" tilewidth="20" tileheight="20" tilecount="160" columns="16">
+ <image source="TS01_worldmap.bmp" trans="5500ff" width="320" height="200"/>
+</tileset>
