99669f5bc75f4cd748eef55aa279e37fbf67c161
julexis <julexislavoie12@gmail.com>
2019-05-05 14:18:35 -0400

    Update Jeu.java

--- My Little Pet Dragon/src/jeu/Jeu.java
+++ My Little Pet Dragon/src/jeu/Jeu.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
-import ObjetsInteractif.MAPS;
+import ObjetsInteractif.*;
 import animal.Joueur;
 public class Jeu extends BasicGame {
 	
@@ -21,6 +21,9 @@ public class Jeu extends BasicGame {
 	private float maxX;
 	private int hauteur;
 	private int largeur;
+	private Dragon drakeDrake;
+	private Jardin jardinGarden;
+	private Lac lakeLake;
 	public Jeu(String title) {
 		super(title);
 		
@@ -34,11 +37,15 @@ public class Jeu extends BasicGame {
 		mapToRender.render(0, 0);
 		joueur.drawMouvement();
 		joueur.drawInvent();
+		joueur.drawOptions();
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		this.joueur=new Joueur(5, 16, 0.2f, 16);
+		drakeDrake=new Dragon();
+		lakeLake=new Lac();
+		jardinGarden=new Jardin();
 		maps=new TiledMap[4];
 		try {
 			maps[MAPS.LAC.toInt()] = new TiledMap("./maps/CarteLac.tmx");
@@ -59,6 +66,8 @@ public class Jeu extends BasicGame {
 		hauteur = 20;
 		largeur = 20;
 		
+		joueur.setObjetEnCours(lakeLake);
+		joueur.setListeInteractionEnCours(lakeLake.getListeInteraction());
 		
 	}
 
@@ -102,6 +111,8 @@ public class Jeu extends BasicGame {
 				mapToRender=maps[MAPS.JARDIN.toInt()];
 				changerMapJoueur(mapToRender);
 			}
+
+			
 		}
 		else if(mapToRender == maps[MAPS.FORET.toInt()])
 		{
@@ -125,6 +136,7 @@ public class Jeu extends BasicGame {
 				joueur.setEmplacementY(hauteur - 1);
 				mapToRender=maps[MAPS.FORET.toInt()];
 				changerMapJoueur(mapToRender);
+				
 			}
 			else if(joueur.getEmplacementX() < 1)
 			{
@@ -136,8 +148,28 @@ public class Jeu extends BasicGame {
 	}
 	public void changerMapJoueur(TiledMap map)
 	{
+		
+		if(map==maps[MAPS.DRAGON.toInt()])
+		{
+			joueur.setListeInteractionEnCours(drakeDrake.getListeInteraction());
+			joueur.setObjetEnCours(drakeDrake);
+		}
+		else if(map==maps[MAPS.LAC.toInt()])
+		{
+			joueur.setListeInteractionEnCours(lakeLake.getListeInteraction());
+			joueur.setObjetEnCours(lakeLake);
+		}
+		else if(map==maps[MAPS.JARDIN.toInt()])
+		{
+			joueur.setListeInteractionEnCours(jardinGarden.getListeInteraction());
+			joueur.setObjetEnCours(jardinGarden);
+		}
+			
+		
 		joueur.setMap(map);
 		joueur.setCalqueBloquer(map.getLayerIndex("bloquer"));
+		joueur.setCalqueInteragir(map.getLayerIndex("interagir"));
 	}
+	
 	}
 	
