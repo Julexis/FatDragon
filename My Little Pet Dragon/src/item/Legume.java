b465e2bc7bcc81771aa57eadfd554c8597b61a1c
julexis <julexislavoie12@gmail.com>
2019-05-05 12:32:11 -0400

    Update Legume.java

--- My Little Pet Dragon/src/item/Legume.java
+++ My Little Pet Dragon/src/item/Legume.java
@@ -1,25 +1,41 @@
 package item;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Legume extends Food{
 	private vegieType type;
 	
 	public Legume(grainType t)
 	{
-		vegieType v = vegieType.applePie;
+		
+		try {
+			vegieType v = vegieType.applePie;
+			setImage(new Image ("./imagesItem/applePie.png"));
 		switch(t)
 		{
 		case blue:
 			v = vegieType.gingerale;
+			setImage(new Image ("./imagesItem/gingerAle.png"));
 			break;
 		case red:
 			v = vegieType.hotdog;
+			setImage(new Image ("./imagesItem/hotdog.png"));
 			break;
 		case green:
 			v = vegieType.orange;
+			setImage(new Image ("./imagesItem/orange.png"));
+		
 			break;
 		}
-		
 		type = v;
+		}
+		catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
 	}
 	
 	public vegieType getType() {
