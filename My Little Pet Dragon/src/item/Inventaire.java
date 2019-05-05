abf052308d7846de0f7fff76ae708abad8253404
julexis <julexislavoie12@gmail.com>
2019-05-05 14:18:29 -0400

    Update Inventaire.java

--- My Little Pet Dragon/src/item/Inventaire.java
+++ My Little Pet Dragon/src/item/Inventaire.java
@@ -9,10 +9,14 @@ public class Inventaire {
 	private Image caseInventaire;
 	private Image imageContour;
 	private boolean isAffiche;
-	private int emplacementXimageFond,emplacementYimageFond,sizeImageFond;
+	private int emplacementXimageFond,emplacementYimageFond,hauteurImageFond, largeurImageFond;
 	private int sizeCaseInventaire;
 	private int selectionX,selectionY;
 	private int sizeContourCase;
+	private int row;
+	private int line; 
+	
+
 	public int getSelectionX() {
 		return selectionX;
 	}
@@ -29,26 +33,56 @@ public class Inventaire {
 		this.selectionY = selectionY;
 	}
 
-	public Inventaire(Image imageFond, Image caseInventaire, Image imageContour) {
+	public Inventaire(Image imageFond, Image caseInventaire, Image imageContour, int row, int line, int emplacementXimageFond, int emplacementYimageFond) {
 		super();
 		this.imageFond = imageFond;
 		this.caseInventaire = caseInventaire;
 		this.imageContour = imageContour;
-		emplacementXimageFond=110;
-		emplacementYimageFond=110;
-		sizeImageFond=200;
-		sizeCaseInventaire=50;
-		sizeContourCase=60;
+		this.emplacementXimageFond = emplacementXimageFond;
+		this.emplacementYimageFond = emplacementYimageFond;
+		sizeContourCase = 30;
+		sizeCaseInventaire=25;
+		this.line=line;
+		this.row=row;
+		hauteurImageFond = (row * sizeCaseInventaire+25)+(row-3)*8;
+		largeurImageFond = (line * sizeCaseInventaire+25)+(line-3)*8;
+		
+		
 	}
 
-	public void actualiser()
-	{
-		if(isAffiche)
-		{
-			imageFond.draw(100,200);
-			
-		}
+
+	public int getEmplacementYimageFond() {
+		return emplacementYimageFond;
 	}
+
+	public void setEmplacementYimageFond(int emplacementYimageFond) {
+		this.emplacementYimageFond = emplacementYimageFond;
+	}
+
+	public int getHauteurImageFond() {
+		return hauteurImageFond;
+	}
+
+	public void setHauteurImageFond(int hauteurImageFond) {
+		this.hauteurImageFond = hauteurImageFond;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
 	public Image getImageFond() {
 		return imageFond;
 	}
@@ -66,13 +100,14 @@ public class Inventaire {
 	}
 	public void Affiche()
 	{
-		imageFond.draw(emplacementXimageFond,emplacementYimageFond,sizeImageFond ,sizeImageFond);
-		imageContour.draw(5+emplacementXimageFond+65*selectionX,5+emplacementYimageFond+65*selectionY,sizeContourCase,sizeContourCase);
-		for (int i=0;i<3;i++)
+		
+		imageFond.draw(emplacementXimageFond, emplacementYimageFond, largeurImageFond, hauteurImageFond);
+		imageContour.draw(2.5f+emplacementXimageFond+32.5f*selectionX,2.5f+emplacementYimageFond+32.5f*selectionY,sizeContourCase,sizeContourCase);
+		for (int i=0;i<line;i++)
 		{
-			for(int j=0;j<3;j++)
+			for(int j=0;j<row;j++)
 			{
-				caseInventaire.draw((emplacementXimageFond+65*i)+10,(emplacementYimageFond+65*j)+10,sizeCaseInventaire,sizeCaseInventaire);
+				caseInventaire.draw((emplacementXimageFond+32.5f*i)+5,(emplacementYimageFond+32.5f*j)+5,sizeCaseInventaire,sizeCaseInventaire);
 			}
 		}
 		
