c42cb4a8caabd36a77f494ec3d3cd8532751ca2d
julexis <julexislavoie12@gmail.com>
2019-05-10 08:43:51 -0400

    Create Interaction.java

---
+++ My Little Pet Dragon/src/Interaction/Interaction.java
@@ -0,0 +1,20 @@
+package Interaction;
+
+import item.Item;
+import ObjetsInteractif.*;
+//Base pour les autres interactions possible. Les Objet interaction appelle une fonction et possède un texte à afficher
+public class Interaction {
+	protected ObjetInteractif structure;
+	
+	public Interaction(ObjetInteractif o)
+	{
+		structure = o;
+	}
+	
+	public void run(Item i){/*Surdéfinir*/}
+	
+	public String getMessage()
+	{
+		return "I'm a regular interraction. Try me!";
+	}
+}
