768d78923a7f9dcf06f00c536b744803990dba3b
julexis <julexislavoie12@gmail.com>
2019-05-10 08:22:25 -0400

    Update Inventaire.java

--- My Little Pet Dragon/src/item/Inventaire.java
+++ My Little Pet Dragon/src/item/Inventaire.java
@@ -1,10 +1,12 @@
 package item;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.*;
 
 public class Inventaire {
 
-	private Item[] items=new Item[8];
+	private ArrayList <Item> items;
 	private Image imageFond;
 	private Image caseInventaire;
 	private Image imageContour;
@@ -32,7 +34,8 @@ public class Inventaire {
 	public void setSelectionY(int selectionY) {
 		this.selectionY = selectionY;
 	}
-
+/*L'inventaire est super dynamique et il est donc possible à l'appelle de choisir le nombre de ligne et de colonne
+ *Il est aussi possible de choisir l'emplacement de création. L'objectif est de créer un inventaire pour le joueur et un inventaire pour le jardin*/
 	public Inventaire(Image imageFond, Image caseInventaire, Image imageContour, int row, int line, int emplacementXimageFond, int emplacementYimageFond) {
 		super();
 		this.imageFond = imageFond;
@@ -46,11 +49,17 @@ public class Inventaire {
 		this.row=row;
 		hauteurImageFond = (row * sizeCaseInventaire+25)+(row-3)*8;
 		largeurImageFond = (line * sizeCaseInventaire+25)+(line-3)*8;
-		
+		items=new ArrayList<Item>();
 		
 	}
 
-
+	public void addObjet(Item i)
+	{
+		if(!(items.size()>=line*row))
+		{
+			items.add(i);
+		}
+	}
 	public int getEmplacementYimageFond() {
 		return emplacementYimageFond;
 	}
@@ -98,9 +107,15 @@ public class Inventaire {
 	public void setAffiche(boolean isAffiche) {
 		this.isAffiche = isAffiche;
 	}
+	public Item getItemSelect()
+	{
+		return items.get(selectionX+selectionY*line);
+	}
 	public void Affiche()
 	{
-		
+		// On dessine l'image de fond à l'emplacement prédéfinie
+		//le contour se fait aussi en conséquence, pareil pour les case qui se font dans une boucle
+		int x=0,y=0;
 		imageFond.draw(emplacementXimageFond, emplacementYimageFond, largeurImageFond, hauteurImageFond);
 		imageContour.draw(2.5f+emplacementXimageFond+32.5f*selectionX,2.5f+emplacementYimageFond+32.5f*selectionY,sizeContourCase,sizeContourCase);
 		for (int i=0;i<line;i++)
@@ -108,6 +123,16 @@ public class Inventaire {
 			for(int j=0;j<row;j++)
 			{
 				caseInventaire.draw((emplacementXimageFond+32.5f*i)+5,(emplacementYimageFond+32.5f*j)+5,sizeCaseInventaire,sizeCaseInventaire);
+			}
+		}
+		for(Item i:items)
+		{
+			i.getImage().draw((emplacementXimageFond+32.5f*x)+10,(emplacementYimageFond+32.5f*y)+10,sizeCaseInventaire-10,sizeCaseInventaire-10);
+			x++;
+			if(x>line)
+			{
+				x=0;
+				y++;
 			}
 		}
 		
