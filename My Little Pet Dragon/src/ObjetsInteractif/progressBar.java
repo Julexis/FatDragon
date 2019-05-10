0837f81e7310a771680b697a9945704a457e7730
julexis <julexislavoie12@gmail.com>
2019-05-10 10:11:03 -0400

    Create progressBar.java

---
+++ My Little Pet Dragon/src/ObjetsInteractif/progressBar.java
@@ -0,0 +1,20 @@
+package ObjetsInteractif;
+
+public enum progressBar {
+	   JAUNE(0),
+	    VERT(1),
+	    ROUGE(2);
+
+	    private int bar = 0;
+
+	    progressBar(int bar)
+	    {
+	        this.bar=bar;
+	    }
+	    public int getBar()
+	    {
+	        return bar;
+	    }
+
+	
+}
