20391af53dece734a231130e8f7203033e9690dc
Sl0ud <49208335+Sl0ud@users.noreply.github.com>
2019-05-10 08:44:41 -0400

    Create Interaction.java

---
+++ My Little Pet Dragon/src/Interaction/Interaction.java
@@ -0,0 +1,14 @@
+package Interaction;
+
+import item.Item;
+import ObjetsInteractif.*;
+//Base pour les autres interactions possible. Les Objet interaction appelle une fonction et possède un texte à afficher
+public class Interaction {
+	
+	public void run(Item i){/*Surdéfinir*/}
+	
+	public String getMessage()
+	{
+		return "I'm a regular interraction. Try me!";
+	}
+}
