64f8252a271674a5fdf2eeaa34a51c80322ff9ff
Sl0ud <49208335+Sl0ud@users.noreply.github.com>
2019-05-10 10:40:34 -0400

    Create SpawnGraine.java

---
+++ My Little Pet Dragon/src/timer/SpawnGraine.java
@@ -0,0 +1,15 @@
+package timer;
+
+public class SpawnGraine extends TimerEvent{
+
+	public SpawnGraine()
+	{
+		super();
+	}
+	
+	@Override
+	public void trigger()
+	{
+		//Écrire code pour ajouter des graines dans la forêt
+	}
+}
